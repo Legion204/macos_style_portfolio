bc9170a88f05942dc6bbd2fd3bb70dde461614bb
Legion204 <ri46801@gmail.com>
2026-01-16 19:29:09 +0600

    add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "#components/Navbar"
+
 const App = () => {
   return (
-    <div>
-      <h1 className="font-bold text-8xl">Hello, World!</h1>
-    </div>
+   <main>
+    <Navbar />
+   </main>
   )
 }
 
